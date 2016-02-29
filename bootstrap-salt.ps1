a4267ccabfd78f5e83df3cfa6728c8c8b4a178f8
Ken Erwin <ken.erwin@kinneygroup.com>
2016-02-28 21:08:01 -0500

    Bootstrap

--- bootstrap-salt.ps1
+++ bootstrap-salt.ps1
@@ -49,131 +49,134 @@ Original Vagrant Provisioner Project -https://github.com/saltstack/salty-vagrant
 Vagrant Project (utilizes this script) - https://github.com/mitchellh/vagrant
 SaltStack Download Location - https://repo.saltstack.com/windows/
 #>
-[CmdletBinding()]
-Param(
-  [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
-  # Doesn't support versions prior to "YYYY.M.R-B" 
-  [ValidatePattern('^(201[0-9]\.[0-9]\.[0-9](\-\d{1})?)$')]
-  [string]$version = '',
+function Install-Salt() {
+    [CmdletBinding()]
+    Param(
+      [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+      # Doesn't support versions prior to "YYYY.M.R-B" 
+      [ValidatePattern('^(201[0-9]\.[0-9]\.[0-9](\-\d{1})?)$')]
+      [string]$version = '',
   
-  [Parameter(Mandatory=$false,ValueFromPipeline=$true)] 
-  [ValidateSet("true","false")] 
-  [string]$runservice = "true",
+      [Parameter(Mandatory=$false,ValueFromPipeline=$true)] 
+      [ValidateSet("true","false")] 
+      [string]$runservice = "true",
 
-  [Parameter(Mandatory=$false,ValueFromPipeline=$true)] 
-  [string]$minion = "salt-minion",
+      [Parameter(Mandatory=$false,ValueFromPipeline=$true)] 
+      [string]$minion = "salt-minion",
   
-  [Parameter(Mandatory=$false,ValueFromPipeline=$true)] 
-  [string]$master = "master"
-)
+      [Parameter(Mandatory=$false,ValueFromPipeline=$true)] 
+      [string]$master = "master"
+    )
 
-Write-Verbose "Parameters passed in:"
-Write-Verbose "version: $version"
-Write-Verbose "runservice: $runservice"
-Write-Verbose "master: $master"
-Write-Verbose "minion: $minion"
+    Write-Verbose "Parameters passed in:"
+    Write-Verbose "version: $version"
+    Write-Verbose "runservice: $runservice"
+    Write-Verbose "master: $master"
+    Write-Verbose "minion: $minion"
 
-If ($runservice.ToLower() -eq "true"){
-  Write-Verbose "Windows service will be set to run"
-  [bool]$runservice = $True
-}
-ElseIf ($runservice.ToLower() -eq "false"){
-  Write-Verbose "Windows service will be stopped and set to manual"
-  [bool]$runservice = $False
-}
-Else {
-  # Param passed in wasn't clear so defaulting to true.
-   Write-Verbose "Windows service defaulting to run automatically"
-  [bool]$runservice = $True
-}
+    If ($runservice.ToLower() -eq "true"){
+      Write-Verbose "Windows service will be set to run"
+      [bool]$runservice = $True
+    }
+    ElseIf ($runservice.ToLower() -eq "false"){
+      Write-Verbose "Windows service will be stopped and set to manual"
+      [bool]$runservice = $False
+    }
+    Else {
+      # Param passed in wasn't clear so defaulting to true.
+       Write-Verbose "Windows service defaulting to run automatically"
+      [bool]$runservice = $True
+    }
 
-# Create C:\tmp\ - if Vagrant doesn't upload keys and/or config it might not exist
-New-Item C:\tmp\ -ItemType directory -force | out-null
+    # Create C:\tmp\ - if Vagrant doesn't upload keys and/or config it might not exist
+    New-Item C:\tmp\ -ItemType directory -force | out-null
 
-# Copy minion keys & config to correct location
-New-Item C:\salt\conf\pki\minion\ -ItemType directory -force | out-null
+    # Copy minion keys & config to correct location
+    New-Item C:\salt\conf\pki\minion\ -ItemType directory -force | out-null
 
-# Check if minion keys have been uploaded
-If (Test-Path C:\tmp\minion.pem) {
-  cp C:\tmp\minion.pem C:\salt\conf\pki\minion\
-  cp C:\tmp\minion.pub C:\salt\conf\pki\minion\
-}
+    # Check if minion keys have been uploaded
+    If (Test-Path C:\tmp\minion.pem) {
+      cp C:\tmp\minion.pem C:\salt\conf\pki\minion\
+      cp C:\tmp\minion.pub C:\salt\conf\pki\minion\
+    }
 
-# Detect architecture
-If ([IntPtr]::Size -eq 4) {
-  $arch = "x86"
-} Else {
-  $arch = "AMD64"
-}
+    # Detect architecture
+    If ([IntPtr]::Size -eq 4) {
+      $arch = "x86"
+    } Else {
+      $arch = "AMD64"
+    }
 
-# If version isn't supplied, use latest.
-if (!$version) {
-    # Find latest version of Salt Minion 
-    $repo = Invoke-Restmethod 'http://repo.saltstack.com/windows/'
-    $regex = "<\s*a\s*[^>]*?href\s*=\s*[`"']*([^`"'>]+)[^>]*?>"
-    $returnMatches = new-object System.Collections.ArrayList
-    $resultingMatches = [Regex]::Matches($repo, $regex, "IgnoreCase")
-    foreach($match in $resultingMatches)
-    {
-        $cleanedMatch = $match.Groups[1].Value.Trim()
-        [void] $returnMatches.Add($cleanedMatch)
-    } 
-    if ($arch -eq 'x86') {$returnMatches = $returnMatches | Where {$_ -like "Salt-Minion*x86-Setup.exe"}}
-    else {$returnMatches = $returnMatches | Where {$_ -like "Salt-Minion*AMD64-Setup.exe"}}
+    # If version isn't supplied, use latest.
+    if (!$version) {
+        # Find latest version of Salt Minion 
+        $repo = Invoke-Restmethod 'http://repo.saltstack.com/windows/'
+        $regex = "<\s*a\s*[^>]*?href\s*=\s*[`"']*([^`"'>]+)[^>]*?>"
+        $returnMatches = new-object System.Collections.ArrayList
+        $resultingMatches = [Regex]::Matches($repo, $regex, "IgnoreCase")
+        foreach($match in $resultingMatches)
+        {
+            $cleanedMatch = $match.Groups[1].Value.Trim()
+            [void] $returnMatches.Add($cleanedMatch)
+        } 
+        if ($arch -eq 'x86') {$returnMatches = $returnMatches | Where {$_ -like "Salt-Minion*x86-Setup.exe"}}
+        else {$returnMatches = $returnMatches | Where {$_ -like "Salt-Minion*AMD64-Setup.exe"}}
     
-    $version = $(($returnMatches | Sort-Object -Descending)[0]).Split('-')[2]
-}
+        $version = $(($returnMatches | Sort-Object -Descending)[0]).Split('-')[2]
+    }
 
-# Download minion setup file
-Write-Output -NoNewline "Downloading Salt minion installer Salt-Minion-$version-$arch-Setup.exe"
-$webclient = New-Object System.Net.WebClient
-$url = "https://repo.saltstack.com/windows/Salt-Minion-$version-$arch-Setup.exe"
-$file = "C:\tmp\salt.exe"
-$webclient.DownloadFile($url, $file)
+    # Download minion setup file
+    Write-Output -NoNewline "Downloading Salt minion installer Salt-Minion-$version-$arch-Setup.exe"
+    $webclient = New-Object System.Net.WebClient
+    $url = "https://repo.saltstack.com/windows/Salt-Minion-$version-$arch-Setup.exe"
+    $file = "C:\tmp\salt.exe"
+    $webclient.DownloadFile($url, $file)
 
-# Install minion silently
-Write-Output -NoNewline "Installing Salt minion"
-#Wait for process to exit before continuing.
-C:\tmp\salt.exe /S /minion-name=$minion /master=$master | Out-Null
+    # Install minion silently
+    Write-Output -NoNewline "Installing Salt minion"
+    #Wait for process to exit before continuing.
+    C:\tmp\salt.exe /S /minion-name=$minion /master=$master | Out-Null
 
 
-# Check if minion config has been uploaded
-If (Test-Path C:\tmp\minion) {
-  cp C:\tmp\minion C:\salt\conf\
-}
+    # Check if minion config has been uploaded
+    If (Test-Path C:\tmp\minion) {
+      cp C:\tmp\minion C:\salt\conf\
+    }
 
-# Wait for salt-minion service to be registered before trying to start it
-$service = Get-Service salt-minion -ErrorAction SilentlyContinue
-While (!$service) {
-  Start-Sleep -s 2
-  $service = Get-Service salt-minion -ErrorAction SilentlyContinue
-}
-
-If($runservice) {
-  # Start service
-  Start-Service -Name "salt-minion" -ErrorAction SilentlyContinue
-
-  # Check if service is started, otherwise retry starting the 
-  # service 4 times.
-  $try = 0
-  While (($service.Status -ne "Running") -and ($try -ne 4)) {
-    Start-Service -Name "salt-minion" -ErrorAction SilentlyContinue
+    # Wait for salt-minion service to be registered before trying to start it
     $service = Get-Service salt-minion -ErrorAction SilentlyContinue
-    Start-Sleep -s 2
-    $try += 1
-  }
+    While (!$service) {
+      Start-Sleep -s 2
+      $service = Get-Service salt-minion -ErrorAction SilentlyContinue
+    }
 
-  # If the salt-minion service is still not running, something probably
-  # went wrong and user intervention is required - report failure.
-  If ($service.Status -eq "Stopped") {
-    Write-Output -NoNewline "Failed to start salt minion"
-    exit 1
-  }
-}
-Else {
-  Write-Output -NoNewline "Stopping salt minion and setting it to 'Manual'"
-  Set-Service "salt-minion" -startupType "Manual"
-  Stop-Service "salt-minion"
-}
+    If($runservice) {
+      # Start service
+      Start-Service -Name "salt-minion" -ErrorAction SilentlyContinue
 
-Write-Output "Salt minion successfully installed"
+      # Check if service is started, otherwise retry starting the 
+      # service 4 times.
+      $try = 0
+      While (($service.Status -ne "Running") -and ($try -ne 4)) {
+        Start-Service -Name "salt-minion" -ErrorAction SilentlyContinue
+        $service = Get-Service salt-minion -ErrorAction SilentlyContinue
+        Start-Sleep -s 2
+        $try += 1
+      }
+
+      # If the salt-minion service is still not running, something probably
+      # went wrong and user intervention is required - report failure.
+      If ($service.Status -eq "Stopped") {
+        Write-Output -NoNewline "Failed to start salt minion"
+        exit 1
+      }
+    }
+    Else {
+      Write-Output -NoNewline "Stopping salt minion and setting it to 'Manual'"
+      Set-Service "salt-minion" -startupType "Manual"
+      Stop-Service "salt-minion"
+    }
+
+    Write-Output "Salt minion successfully installed"
+}
+Install-Salt
